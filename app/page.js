a4ed8999c40ba7b344624ae4dad1bf51194f1615
Salman Ahmad <160137386+SalmanAhmad-code@users.noreply.github.com>
2024-08-15 17:57:24 +0000

    err fix 3

--- app/page.js
+++ app/page.js
@@ -19,11 +19,8 @@ export default function Home() {
       <Box id="about" className="press-start-2p-regular Section2 h-fit relative bg-cover bg-center w-full flex flex-col items-center justify-center text-center p-8">
         <h1 className="mt-3 pb-8 w-fit border-b-2 border-white text-3xl md:text-5xl font-bold text-[#FFEA20]">About</h1>
         <Box className="Section2-Content h-full relative flex flex-col justify-between items-center">
-          <p className="text-white text-sm md:text-2xl self-start mt-8 w-full md:w-3/5">
+          <p className="text-white text-sm md:text-2xl mt-8 w-full md:w-3/5">
             GameFlip is your ultimate companion for mastering the world of gaming. Whether you&apos;re a casual gamer or a competitive pro, our AI-driven flashcards are designed to help you sharpen your skills, enhance your knowledge, and discover new aspects of your favorite games.
-          </p>
-          <p className="text-white text-sm md:text-2xl self-end mt-4 md:mt-8 w-full md:w-3/5">
-            With GameFlip, learning becomes a game in itself. Our platform curates and delivers personalized flashcards that cover everything from game mechanics and strategies to hidden lore and trivia. Whether you're preparing for an in game challenge or just looking to expand your gaming horizons, GameFlip has you covered.
           </p>
           <p className="text-white text-sm md:text-xl mt-4 md:mt-8 w-full md:w-3/5">
             Join us on our mission to transform the way gamers learn and connect. Level up your gaming experience with GameFlip!
