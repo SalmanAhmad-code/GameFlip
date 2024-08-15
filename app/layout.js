237afb3b359f5d79bd404f192f5c409f92ec95e6
Salman Ahmad <160137386+SalmanAhmad-code@users.noreply.github.com>
2024-08-15 17:47:45 +0000

    err fixed

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "./components/NavBar";
 import NavBar2 from "./components/NavBar2";
-import ConstructionTag from "./components/ConstructionTag";
 import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
